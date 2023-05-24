31ef96f1b2502f0ecf3acc7c054b8e87d919e6ad
Rafa Osnaya <osnayadeluciorafael@hotmail.com>
2023-05-24 11:14:35 -0600

    Inicio SingUp

--- core/authentication/serializers.py
+++ core/authentication/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 from .models import CustomUser
 
+
 class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField(required = True)
-    password = serializers.CharField(required = True, write_only=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(required=True, write_only=True)
+
     class Meta:
         model = CustomUser
-        fields =  ['id','first_name', 'last_name', 'email', 'phone_number', 'last_login', 'date_joined']
+        fields = '__all__'
+
+
+class SingUpSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    phone_number = serializers.EmailField(required=True)
--- core/authentication/views.py
+++ core/authentication/views.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import LoginSerializer, UserSerializer
+from .serializers import LoginSerializer, UserSerializer, SingUpSerializer
 
 
 # Create your views here.
@@ -18,29 +18,37 @@ class LoginView(APIView):
 
     # def get(self, request):
     #     return Response({}, status.HTTP_400_BAD_REQUEST)
-    
-
-    serializer_class = LoginSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        
+
         user = authenticate(
-                    request,
-                    email = serializer.validated_data['email'],
-                    password = serializer.validated_data['password']
-                )
+            request,
+            email=serializer.validated_data['email'],
+            password=serializer.validated_data['password']
+        )
         if user is not None:
             user.last_login = datetime.now()
             user.save()
             user_serializer = UserSerializer(user)
-            return Response(user_serializer.data, status= status.HTTP_200_OK)
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(
                 {
                     "error": "401 Unauthorized",
                     "message": "The credentials  provided are not valid"
                 },
-                status = status.HTTP_401_UNAUTHORIZED
+                status=status.HTTP_401_UNAUTHORIZED
             ),
+
+
+class SingUpView(APIView):
+    serializer_class = SingUpSerializer
+
+    def post(self, request):
+        print(request.data)
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
